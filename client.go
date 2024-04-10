8fd7bb862f0abe2f0bb75e084baaf867b65991d5
Baurzhan <220476@astanait.edu.kz>
2024-04-10 16:33:22 +0600

    Final version with history func

---
+++ client.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+const (
+	CONN_HOST        = "localhost"
+	CONN_PORT        = ":3335"
+	CONN_TYPE        = "tcp"
+	MSG_DISCONNECT   = "Disconnected from the server.\n"
+	MSG_HISTORY      = "/history"
+	HISTORY_RESPONSE = "Chat history:\n"
+	HISTORY_FILENAME = "chat_history.txt"
+)
+
+func Read(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	for {
+		str, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf(MSG_DISCONNECT)
+			return
+		}
+		fmt.Print(str)
+	}
+}
+
+func Write(conn net.Conn, name string) {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(conn)
+
+	for {
+		str, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if strings.TrimSpace(str) == MSG_HISTORY {
+			_, err = writer.WriteString(MSG_HISTORY + "\n")
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			err = writer.Flush()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			continue
+		}
+
+		_, err = writer.WriteString(name + ": " + str)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		err = writer.Flush()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+}
+
+func main() {
+	fmt.Print("Enter your name: ")
+	reader := bufio.NewReader(os.Stdin)
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+
+	conn, err := net.Dial(CONN_TYPE, CONN_HOST+CONN_PORT)
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to the server.")
+	go Read(conn)
+	Write(conn, name)
+}
---
+++ server.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"time"
+)
+
+const (
+	CONN_PORT           = ":3335"
+	CONN_TYPE           = "tcp"
+	MSG_HISTORY_REQUEST = "/history"
+	HISTORY_FILENAME    = "chat_history.txt"
+)
+
+var (
+	clients     = make(map[net.Conn]bool)
+	broadcast   = make(chan string)
+	joinChannel = make(chan net.Conn)
+)
+
+func main() {
+	listener, err := net.Listen(CONN_TYPE, CONN_PORT)
+	if err != nil {
+		log.Println("Error: ", err)
+		os.Exit(1)
+	}
+	defer listener.Close()
+	log.Println("Listening on " + CONN_PORT)
+
+	go broadcaster()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error: ", err)
+			continue
+		}
+		clients[conn] = true
+		go handleClient(conn)
+	}
+}
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	joinChannel <- conn
+	reader := bufio.NewReader(conn)
+
+	for {
+		str, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println("Client disconnected.")
+			delete(clients, conn)
+			return
+		}
+
+		str = strings.TrimSpace(str)
+
+		if str == MSG_HISTORY_REQUEST {
+			sendChatHistory(conn)
+			continue
+		}
+
+		log.Println("Received:", str)
+
+		// Save the message to chat_history.txt
+		saveToChatHistory(str)
+
+		time := time.Now().Format(time.ANSIC)
+		responseStr := fmt.Sprintf("[%v] %v", time, str)
+		broadcast <- responseStr
+	}
+}
+
+func saveToChatHistory(message string) {
+	file, err := os.OpenFile(HISTORY_FILENAME, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		log.Println("Error opening history file:", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = fmt.Fprintf(file, "%s\n", message)
+	if err != nil {
+		log.Println("Error writing to history file:", err)
+		return
+	}
+}
+
+func broadcaster() {
+	for {
+		select {
+		case msg := <-broadcast:
+			for client := range clients {
+				_, err := client.Write([]byte(msg + "\n"))
+				if err != nil {
+					log.Println("Error broadcasting message:", err)
+					delete(clients, client)
+					client.Close()
+				}
+			}
+		case <-joinChannel:
+			log.Println("New client joined.")
+		}
+	}
+}
+
+func sendChatHistory(conn net.Conn) {
+	file, err := os.Open(HISTORY_FILENAME)
+	if err != nil {
+		log.Println("Error opening history file:", err)
+		conn.Write([]byte("No chat history available.\n"))
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		conn.Write([]byte(line + "\n"))
+	}
+}
